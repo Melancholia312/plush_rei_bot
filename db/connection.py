992a9552862d69e653cbb9b03c591fba585edd32
Melancholia312 <67428510+Melancholia312@users.noreply.github.com>
2021-11-24 20:45:18 +0300

    Update connection.py

--- db/connection.py
+++ db/connection.py
@@ -1,12 +1,23 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import urllib.parse
+
+result = urllib.parse.urlparse("postgres://qpobxihgxvpgun:0466a9f70793348cb8bbba02592318898c1d3917792e4da5363ed8115ad7d0d6@ec2-3-248-87-6.eu-west-1.compute.amazonaws.com:5432/d3ogdamab7jk17")
+username = result.username
+password = result.password
+database = result.path[1:]
+hostname = result.hostname
+port = result.port
 
 
 def get_connect():
-    connection = psycopg2.connect(user='postgres',
-                                  password='TombRaider2003',
-                                  host='127.0.0.1',
-                                  port='5432',
-                                  database="PlushReiDB")
+    connection = psycopg2.connect(
+        database=database,
+        user=username,
+        password=password,
+        host=hostname,
+        port=port
+    )
     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     return connection
+
